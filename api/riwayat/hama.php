b1485fd5e66fc84c3073495eb7b12d563fc9cb57
vitajofita@gmail.com <vitajofita@gmail.com>
2025-06-20 13:04:05 +0800

    Hapus

--- api/riwayat/hama.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../../config/database.php';
-require_once '../../helpers/response.php';
-
-$stmt = $pdo->query("SELECT * FROM riwayat WHERE jenis = 'hama' ORDER BY created_at DESC");
-$data = $stmt->fetchAll();
-json_response($data);
